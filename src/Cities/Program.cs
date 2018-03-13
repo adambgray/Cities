abd314851055c6394a817c825cf1fd7f5d6df4d8
Adam Gray <adambgray@gmail.com>
2018-03-13 19:02:53 -0400

    implemented compare by State, Population, and Area

--- src/Cities/Program.cs
+++ src/Cities/Program.cs
@@ -11,13 +11,39 @@ namespace Cities
             List<City> cities = CityDataImporter.LoadData();
 
             // TODO Swap out comparers as desired
-            IComparer<City> comparer = new NameComparer();
+            IComparer<City> namecomparer = new NameComparer();
 
-            cities.Sort(comparer);
+            cities.Sort(namecomparer);
 
             PrintCities(cities);
 
             Console.ReadLine();
+
+            IComparer<City> statecomparer = new StateComparer();
+
+            cities.Sort(statecomparer);
+
+            PrintCities(cities);
+
+            Console.ReadLine();
+
+            IComparer<City> popcomparer = new PopulationComparer();
+
+            cities.Sort(popcomparer);
+
+            PrintCities(cities);
+
+            Console.ReadLine();
+
+            IComparer<City> areacomparer = new AreaComparer();
+
+            cities.Sort(areacomparer);
+
+            PrintCities(cities);
+
+            Console.ReadLine();
+
+
         }
 
         private static void PrintCities(List<City> cities)
